bc261d8efb805919f12b249d4960ca79f6502cb7
slackjaww <yac@live.com.ar>
2019-12-12 04:22:39 -0300

    iniciar / cerrar sesion

---
+++ docs/despedida.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	 <meta http-equiv="refresh" content="3;URL=login.php">
+</head>
+<body>
+
+	<h1>SE CERRO SESIÓN</h1>
+	<h4>Se redirigira automatimante al login en 3seg</h4>
+	<?php
+			setcookie("emailusuario", "", -1);	?>
+
+</body>
+</html>
--- docs/funciones/navbar.php
+++ docs/funciones/navbar.php
@@ -4,6 +4,40 @@
     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
 </button>
+
+
+<?php 
+if(isset($_COOKIE["emailusuario"])){ session_start();
+?>
+
+<div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="juego.php"><?php echo $_SESSION["nombre"] . ", jugá ahora!"; ?></a>
+        </li>
+                <li class="nav-item">
+            <a class="nav-link" href="index.php">HOME</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="ranking-de-usuarios.php">RANKING</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="contacto.php">CONTACTO</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="preguntas-frecuentes.php">AYUDA</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="admin.php">ADMIN</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="despedida.php">CERRAR SESION</a>
+        </li>
+
+    </ul>
+</div>
+<?php 
+}else{ setcookie("emailusuario", "", -1); ?>
 <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
@@ -24,13 +58,12 @@
         <li class="nav-item">
             <a class="nav-link" href="admin.php">ADMIN</a>
         </li>
-
-
-        <!--//////////////////// ELIMINAR DESPUES DE QUE SEA FUNCIONAL ////////////////////////////-->
         <li class="nav-item">
-            <a class="nav-link" href="juego.php">EJEMPLO</a>
+            <a class="nav-link" href="login.php">INGRESAR</a>
         </li>
 
     </ul>
 </div>
+<?php }?>
+
 </nav>

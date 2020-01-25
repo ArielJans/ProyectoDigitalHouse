f9c0e1059ba3c909d750fe85970bf97064816ceb
adrian <adrianbogado67@gmail.com>
2020-01-25 12:52:42 -0300

    cambios en codigo de register

---
+++ docs/includes/edit-user-info.inc.php
@@ -0,0 +1,93 @@
+<?php
+$currentEmail = $_SESSION['user']['email'];
+$MYSQL_DB = new MYSQL_DB();
+
+if (isset($_POST['userName'])) {
+  $userName = $_POST['userName'];
+
+  $errorUserName = validateUserName($userName);
+
+  if ($userName === $_SESSION['user']['user_name']) {
+    $errorUserName = "No se ha cambiado nada!";
+  }
+
+  if ($errorUserName === "") {
+    $_SESSION['user']['user_name'] = $userName;
+
+    $MYSQL_DB->updateUser_userName($userName, $_SESSION['user']['id']);
+  }
+}
+
+
+if (isset($_POST['email'])) {
+  $email = $_POST['email'];
+
+  $errorEmail = validateEmail($email);
+
+  if ($email === $_SESSION['user']['email']) {
+    $errorEmail = "No se ha cambiado nada!";
+  }
+
+  if ($errorEmail === "") {
+    if (isset($_COOKIE['login'])) {
+      setcookie('login', 'true', time() + 60*60*24*30);
+      setcookie('user', $email, time() + 60*60*24*30);
+    }
+    $_SESSION['user']['email'] = $email;
+    $MYSQL_DB->updateUser_email($email, $_SESSION['user']['id']);
+  }
+}
+
+
+if ($_FILES && $_FILES['userPic']['error'] !== 4) {
+  $userPic = $_FILES['userPic'];
+
+  $errorUserPic = validateUserPic($userPic);
+
+  if ($errorUserPic === "") {
+    $currentPicDirectory = "images/users/" . $_SESSION['user']['user_pic'];
+    unlink($currentPicDirectory);
+
+    $picToken = bin2hex(random_bytes(5));
+    $userPicName = $picToken . $currentEmail . "." . pathinfo($userPic['name'], PATHINFO_EXTENSION);
+    $_SESSION['user']['user_pic'] = $userPicName;
+
+    $MYSQL_DB->updateUser_userPic($userPicName, $_SESSION['user']['id']);
+
+    move_uploaded_file($userPic['tmp_name'],"images/users/" . $userPicName);
+  }
+}
+
+if (isset($_POST['newPassword'])) {
+  $newPassword = $_POST['newPassword'];
+  $newPasswordRepeat = $_POST['newPasswordRepeat'];
+
+  $errorNewPassword = validatePassword($newPassword);
+
+  if ($newPassword !== $newPasswordRepeat) {
+    $errorNewPassword = "Las contraseñas no coinciden";
+  }
+
+  $confirmationPass = "";
+  if (!password_verify($_POST['password'], $_SESSION['user']['password'])) {
+    $confirmationPass = "Contraseña incorrecta";
+  }
+  if ($errorNewPassword === "" && $confirmationPass === "") {
+    $hashedPass = password_hash($newPassword, PASSWORD_DEFAULT);
+    $_SESSION['user']['password'] = $hashedPass;
+
+    $MYSQL_DB->updateUser_password($hashedPass, $_SESSION['user']['id']);
+  }
+}
+
+
+$usersInJSON = getUsersJSON();
+foreach ($usersInJSON as $key => $userInJSON) {
+  if ($userInJSON['email'] === $currentEmail) {
+    $usersInJSON[$key] = $_SESSION['user'];
+    break;
+  }
+}
+
+$usersInJSON = json_encode($usersInJSON);
+file_put_contents("json/users.json", $usersInJSON);
--- docs/register.php
+++ docs/register.php
@@ -1,153 +1,39 @@
 <?php
+include_once 'core/init.php';
 
-$errores = [];
-$nombre = "";
-$email = "";
-$password = "";
-$repassword = "";
-$salida = 2;
-$elUsuario = [];
-$extension = "";
-$imagentemporal = "";
+loginByCookie();
 
-if($_POST)
-{
-	if(isset($_POST["nombre"]))
-	{
-		if(empty($_POST["nombre"]))
-		{
-			$errores["nombre"] = "Ténes que ingresar un nombre! lo dejaste en blanco";
-		}
-		if(strlen($_POST["nombre"]) < 3 && !empty($_POST["nombre"]))
-		{
-			$errores["nombre"] = "El nombre debe tener más de dos caracteres!";
-		}
-		else
-		{
-			$nombre = $_POST["nombre"];
-		}
-	}
-	if(isset($_POST["email"]))
-	{
-		if(empty($_POST["email"]))
-		{
-			$errores["email"] = "Ténes que ingresar un email! lo dejaste en blanco";
-		}
-		if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) && !empty($_POST["email"]))
-		{
-			$errores["email"] = "Lo que pones acá debe ser un email valido, no olvides poner un arroba y poner un dominio valido";
-		}
-		else
-		{
-			$email = $_POST["email"];
-		}
-	}
-
-	if(isset($_POST["password"]))
-	{
-		if(empty($_POST["password"]))
-		{
-			$errores["password"] = "Tenes que poner una contraseña! lo dejaste en blanco";
-		}
-		if(strlen($_POST["password"]) < 6 && !empty($_POST["password"]))
-		{
-			$errores["password"] = "Su contraseña debe tener por lo menos seis caracteres, agregale más";
-			$repassword = "";
-			$password = "";
-
-		}
-		else
-		{
-			$password = $_POST["password"];
-		}
-	}
-
-	if(isset($_POST["repassword"]))
-	{
-		if(empty($_POST["repassword"]))
-		{
-			$errores["repassword"] = "Este campo tiene que estar lleno, cargalo!";
-		}
-		if(strcmp($_POST["repassword"], $_POST["password"]) && !empty($_POST["repassword"]))
-		{
-			$errores["repassword"] = "Contraseñas no coinciden, favor de verificar";
-			$repassword = "";
-
-		}
-		if (empty($password))
-		{
-			$repassword = "";
-		}
-		else
-		{
-			$repassword = $_POST["repassword"];
-		}
-	}
-	if (count($errores) === 0)
-	{
-		$salida = 0;
-		if($_FILES)
-		{
-			$extension = pathinfo($_FILES["avatar"]["name"], PATHINFO_EXTENSION);	//para tener la extensión del archivo
-			$nombredelproyecto = dirname(__FILE__);	//para saber la ruta del archivo .php actual
-			$imagentemporal = $_FILES["avatar"]["tmp_name"];
-			$nombredelproyecto = $nombredelproyecto . "\img\avatares";
-			$nombredelproyecto = $nombredelproyecto . "\avatar_" . uniqid() . "." . $extension; //darle un nombre unico
-			move_uploaded_file($imagentemporal, $nombredelproyecto); //guardar la imagen en el servidor
-		}
-		if (count($errores) === 0)
-		{
-			$elUsuario = [
-				"nombre" => trim($nombre),
-				"email" => trim($email),
-				"pass" => password_hash($password, PASSWORD_DEFAULT)
-			];
-
-
-			if(!empty($_FILES["avatar"]["name"]))
-			{
-				$elUsuario["avatar"] = $nombredelproyecto;
-			}
-
-			$usuarioenjson = json_encode($elUsuario);
-			file_put_contents("usuarios.json", $usuarioenjson . PHP_EOL, FILE_APPEND);
-			session_start();
-			$_SESSION["nombre"] = $elUsuario["nombre"];
-			$_SESSION["email"] = $elUsuario["email"];
-			if(!empty($_FILES["avatar"]["name"]))
-			{
-				$_SESSION["avatar"] = $nombredelproyecto;
-			}
-
-			header("Location: login.php");
-			exit;
-
-		}
-		else
-		{
-			$salida = 1;
-		}
-		if(!empty($_POST["recordarme"]))
-		{
-			// echo "usuario quiere recordar";
-			$password = $_POST["password"];
-			$repasword = $_POST["repassword"];
-			$email = $_POST["email"];
-			$nombre = $_POST["nombre"];
-		}
-		else
-		{
-			// echo "usuario no quiere recordar";
-			$password = "";
-			$repassword = "";
-			$email = "";
-			$nombre = "";
-		}
-
-	}
+if (isset($_SESSION['login'])) {
+  header("Location: index.php");exit;
 }
 
-?>
+$valid = new Validator();
+
+  if ($_POST) {
+    $valid->validateEmail($_POST["email"]);
+
+    $valid->validatePassword($_POST['password']);
+
+    $valid->validateZip($_POST['zip']);
+
+    $valid->validateUserPic($_FILES['userPic']);
+
+    if (empty($valid->getErrors())) {
+      $OBJ_user = new User(0);
+      $OBJ_user->register();
+      header("Location: login.php");
+    }else{
+      $errors = $valid->getErrors();
+    }
+  }
+
+ ?>
+
+<?php
+
+  $bs = 1;
+
+ ?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -164,7 +50,7 @@ if($_POST)
 <body>
 	<!--//////////////////// INICIO HEADER/NAV ////////////////////////////-->
 	<header>
-		<?php include("funciones/navbar.php");?>
+		<?php include("partes/navbar.php");?>
 	</header>
 	<!--//////////////////// FIN HEADER/NAV ////////////////////////////-->
 	<main>
@@ -173,26 +59,23 @@ if($_POST)
 			<form class="form-signin" action="" method="POST" enctype="multipart/form-data">
 				<img class="mb-4 logo" src="img/medal.png" alt="">
 				<h1 class="h3 mb-3 font-weight-normal">Registrarse<?php// var_dump($_POST);?></h1>
-				<input type="text" id="inputName" name="nombre" class="form-control " placeholder="Nombre" value="<?php if(empty($_POST["nombre"])){echo"";}else{echo $nombre;}?>"> <?php //se modifica $_POST["nombre"] por $nombre ?>
-				<small class=""><?=(isset($errores["nombre"])) ? $errores["nombre"] : "" ?></small>
-				<input type="email" id="inputEmail" name="email" class="form-control " placeholder="Email" value="<?php if(empty($_POST["email"])){echo"";}else{echo $email;}?>"> <?php //se modifica $_POST["email"] por $email ?>
-				<small class=""><?= (isset($errores["email"])) ? $errores["email"] : "" ?></small>
-				<input type="password" id="inputPassword" name="password" class="form-control " placeholder="Contraseña" value="<?php 
-				if(isset($errores["password"])){echo"";}else{echo $password;}?>">
-				<small class=""><?= (isset($errores["password"])) ? $errores["password"] : "" ?></small>
-				<input type="password" id="rePassword" name="repassword" class="form-control " placeholder="Repetir Contraseña" value="<?php 
-				if(isset($errores["repassword"])){echo"";}else{echo $repassword;}?>">
-				<small class="text-center"><?= (isset($errores["repassword"])) ? $errores["repassword"] : "" ?></small>
+				<div class="form-group col-md-6">
+        <label for="email">Email*</label> <?php if(isset($errors['email'])){echo '<span>'. $errors['email'] .'</span>';} ?>
+        <input type="email" class="form-control" name="email" id="email" placeholder="Email" value="<?php if($_POST){echo $_POST['email']; }?>">
+      </div>
+			<div class="form-group col-md-6">
+			<label for="password">Contraseña*</label> <?php if(isset($errors['password'])){echo '<span>'. $errors['password'] .'</span>';} ?>
+			<input type="password" class="form-control" name="password" id="password" placeholder="***********">
+		</div>
 
 				<!--///////////////////////-->
-				<p class="avatar-t">Agregar avatar:</p>
-				
-				<div class="avatar-file">
-					<input type="file" class=""   name="avatar">
-				</div>
+				<div class="form-group col-md-2">
+        <label for="userPic">Foto de perfil</label>
+        <input type="file" name="userPic" id="userPic">
+        <?php if(isset($errors['userPic'])){echo '<span class="zip-error">'. $errors['userPic'] .'</span>';} ?>
+      </div>
 				<!--///////////////////////-->
 
-				<p><input type="checkbox" class="chek" value="remember-me" name="recordarme" <?php if(isset($_POST["recordarme"])){echo "checked";}else{echo "";}?>> Recordarme</p>
 				<button class="btn btn-lg btn-primary btn-block" type="submit">Registrarme</button>
 				<p class="mt-5 mb-3 text-muted">Ya estas registrado? <a href="login.php">Ingresar</a></p>
 			</form>
